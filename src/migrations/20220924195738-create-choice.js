6af87c3d293a32b35216f68111ecf308ba420364
Chris Hile <chile@student.fullsail.edu>
2022-09-24 16:05:40 -0400

    setup

---
+++ src/migrations/20220924195738-create-choice.js
@@ -0,0 +1,46 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Choices', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      a: {
+        type: Sequelize.STRING
+      },
+      b: {
+        type: Sequelize.STRING
+      },
+      c: {
+        type: Sequelize.STRING
+      },
+      d: {
+        type: Sequelize.STRING
+      },
+      questionId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: {
+            tableName: 'Questions'
+          },
+          key: 'id'
+        },
+        allowNull: true
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Choices');
+  }
+};
---
+++ src/models/question.js
@@ -0,0 +1,25 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Question extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      models.Question.belongsTo(models.Quiz)
+      models.Question.hasMany(models.Choice)
+    }
+  }
+  Question.giinit({
+    question: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Question',
+  });
+  return Question;
+};
